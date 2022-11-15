b973f0d180110a3f1a279c5d47004547637d394d
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2022-11-15 13:02:44 +0100

    Fix userlist bug in mobile

--- public/users.mjs
+++ public/users.mjs
@@ -7,37 +7,44 @@ export function sendUserName(socket) {
   let color = userParams.getAll("color").toString();
   let food = userParams.getAll("food").toString();
   if (!food) {
-    food = "turnip"
+    food = "turnip";
   }
   if (!color) {
-    color = "anonymous"
+    color = "anonymous";
   }
-  let name = color.charAt(0).toUpperCase() + color.slice(1) + food.charAt(0).toUpperCase() + food.slice(1);
-  socket.emit("connected", name)
+  let name =
+    color.charAt(0).toUpperCase() +
+    color.slice(1) +
+    food.charAt(0).toUpperCase() +
+    food.slice(1);
+  socket.emit("connected", name);
 }
 
 //döljer och visar listan i mobilläget
 export function toggleUsers() {
-  let users = document.querySelectorAll(".userNames");
-  for (let index = 0; index < users.length; index++) {
-    users[index].classList.toggle("hidden")
-  };
+const list = document.querySelector(".list");
+list.classList.toggle("hidden");
 }
-
-export function printUserList(users) {userList.innerHTML = "";
-const heading = document.createElement("li");
-heading.textContent = "Who is here?";
-heading.id = "usersHeading"
-userList.appendChild(heading);
-for (let user in users) {
-  const li = document.createElement("li");
-  li.classList.add("hidden")
-  li.classList.add("userNames")
-  const userName = document.createTextNode(users[user].userName);
-  li.appendChild(userName);
-  userList.appendChild(li);
+ 
+export function printUserList(users) {
+  userList.innerHTML = "";
+  const heading = document.createElement("h1");
+  heading.textContent = "Who is here?";
+  heading.id = "usersHeading";
+  userList.appendChild(heading);
+  const list = document.createElement("ul");
+  list.classList.add("hidden");
+  list.classList.add("list");
+  for (let user in users) {
+    const li = document.createElement("li");
+    li.classList.add("userNames");
+    const userName = document.createTextNode(users[user].userName);
+    li.appendChild(userName);
+    list.appendChild(li);
+  }
+  userList.appendChild(list);
+  userList.addEventListener("click", (e) => {
+    e.preventDefault;
+    toggleUsers();
+  });
 }
-userList.addEventListener("click", (e) => {
-  e.preventDefault;
-  toggleUsers()
-})}
